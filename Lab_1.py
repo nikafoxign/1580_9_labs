3db4114599571a00bdf4e56a78a4cca9c50856b5
NikaFox <134002982+nikafoxign@users.noreply.github.com>
2025-11-26 13:53:09 +0600

    Update Lab_1.py

--- Lab_1.py
+++ Lab_1.py
@@ -1,95 +1,128 @@
-def main():
-    TimeInList = input().split()
-    if len(TimeInList)!=2:
-        print("Не корректное количество аргументов.")
-        return 
-    Hours = int(TimeInList[0])
-    Minute = int(TimeInList[1])
+TimeInList = input().split()
+if len(TimeInList) != 2:
+    print("Не корректное количество аргументов.")
+    exit()
+
+Hours = int(TimeInList[0])
+Minute = int(TimeInList[1])
 
 
-    def IsCorrect(InHours, InMinute):
-        if InHours > 23 or InHours < 0:
-            print("Введены недопустимые данные: часы должны быть от 0 до 23.")
-            return False
-        if InMinute > 59 or InMinute< 0:
-            print("Введены недопустимые данные: минуты должны быть от 0 до 59.")
-            return False
-        return True
-
-    def IsExept(InHours,InMinute):
-        if InHours==00 and InMinute == 00:
-            return "полночь"
-        if InHours == 12 and InMinute == 00:
-            return "полдень"
+def IsCorrect(InHours, InMinute):
+    if InHours > 23 or InHours < 0 or TimeInList[0] != 0:
+        print("Введены недопустимые данные: часы должны быть от 0 до 23.")
         return False
-
-    def TextHours(InHours):
-        FinalTextHours=0
-        if InHours==0:
-            FinalTextHours = "часов"
-        if InHours == 1:
-            FinalTextHours = "час"
-        if 1 < InHours < 5:
-            FinalTextHours = "часа"
-        if 4 < InHours < 21:
-            FinalTextHours = "часов"
-        if InHours == 21:
-            FinalTextHours = "час"
-        if 22 < InHours < 24:
-            FinalTextHours = "часа"
+    if InMinute > 59 or InMinute < 0:
+        print("Введены недопустимые данные: минуты должны быть от 0 до 59.")
+        return False
+    return True
 
 
-        return str(InHours)+' '+FinalTextHours
+def IsExept(InHours, InMinute):
+    if InHours == 00 and InMinute == 00:
+        return "полночь"
+    if InHours == 12 and InMinute == 00:
+        return "полдень"
+    return False
 
-    def TextMinute(InMinute):
-        FinalTextMinute=0
-        if InMinute==0:
-            return ""
 
-        match InMinute:
-           case 1: FinalTextMinute = "минута"
-           case 21: FinalTextMinute = "минута"
-           case 31: FinalTextMinute = "минута"
-           case 41: FinalTextMinute = "минута"
-           case 51: FinalTextMinute = "минута"
+def TextHours(InHours):
+    FinalTextHours = 0
+    if InHours == 0:
+        FinalTextHours = "часов"
+    if InHours == 1:
+        FinalTextHours = "час"
+    if 1 < InHours < 5:
+        FinalTextHours = "часа"
+    if 4 < InHours < 21:
+        FinalTextHours = "часов"
+    if InHours == 21:
+        FinalTextHours = "час"
+    if 22 < InHours < 24:
+        FinalTextHours = "часа"
 
-           case 2: FinalTextMinute = "минуты"
-           case 22: FinalTextMinute = "минуты"
-           case 32: FinalTextMinute = "минуты"
-           case 42: FinalTextMinute = "минуты"
-           case 52: FinalTextMinute = "минуты"
+    return str(InHours) + ' ' + FinalTextHours
 
-           case 3: FinalTextMinute = "минуты"
-           case 23: FinalTextMinute = "минуты"
-           case 33: FinalTextMinute = "минуты"
-           case 43: FinalTextMinute = "минуты"
-           case 53: FinalTextMinute = "минуты"
 
-           case 4: FinalTextMinute = "минуты"
-           case 24: FinalTextMinute = "минуты"
-           case 34: FinalTextMinute = "минуты"
-           case 44: FinalTextMinute = "минуты"
-           case 54: FinalTextMinute = "минуты"
+def TextMinute(InMinute):
+    FinalTextMinute = 0
+    if InMinute == 0:
+        return ""
 
-           case _: FinalTextMinute = "минут"
-        return ' '+str(InMinute)+' '+FinalTextMinute
+    match InMinute:
+        case 1:
+            FinalTextMinute = "минута"
+        case 21:
+            FinalTextMinute = "минута"
+        case 31:
+            FinalTextMinute = "минута"
+        case 41:
+            FinalTextMinute = "минута"
+        case 51:
+            FinalTextMinute = "минута"
 
-    def TextDay(InHours):
-        if 0<InHours<6:
-            return "ночи"
-        if 5<InHours<12:
-            return "утра"
-        if 11<InHours<18:
-            return "дня"
-        if 17<InHours<24:
-            return "вечера"
+        case 2:
+            FinalTextMinute = "минуты"
+        case 22:
+            FinalTextMinute = "минуты"
+        case 32:
+            FinalTextMinute = "минуты"
+        case 42:
+            FinalTextMinute = "минуты"
+        case 52:
+            FinalTextMinute = "минуты"
 
-    def TextMinute00(InMinute):
-        if InMinute==0: return "ровно"
-        else: return ""
-    if IsCorrect(Hours,Minute):
-        if IsExept(Hours,Minute):print(IsExept(Hours,Minute))
-        else: print(TextHours(Hours)+TextMinute(Minute)+" "+TextDay(Hours)+" "+TextMinute00(Minute))
-    pass
+        case 3:
+            FinalTextMinute = "минуты"
+        case 23:
+            FinalTextMinute = "минуты"
+        case 33:
+            FinalTextMinute = "минуты"
+        case 43:
+            FinalTextMinute = "минуты"
+        case 53:
+            FinalTextMinute = "минуты"
+
+        case 4:
+            FinalTextMinute = "минуты"
+        case 24:
+            FinalTextMinute = "минуты"
+        case 34:
+            FinalTextMinute = "минуты"
+        case 44:
+            FinalTextMinute = "минуты"
+        case 54:
+            FinalTextMinute = "минуты"
+
+        case _:
+            FinalTextMinute = "минут"
+    return ' ' + str(InMinute) + ' ' + FinalTextMinute
+
+
+def TextDay(InHours):
+    if 0 <= InHours < 6:
+        return "ночи"
+    if 6 <= InHours < 12:
+        return "утра"
+    if 12 <= InHours < 18:
+        return "дня"
+    if 18 <= InHours < 24:
+        return "вечера"
+
+
+def TextMinute00(InMinute):
+    if InMinute == 0:
+        return "ровно"
+    else:
+        return ""
+
+
+def main():
+    if IsCorrect(Hours, Minute):
+        if IsExept(Hours, Minute):
+            print(IsExept(Hours, Minute))
+        else:
+            print(TextHours(Hours) + TextMinute(Minute) + " " + TextDay(Hours) + " " + TextMinute00(Minute))
+
+
 if __name__ == "__main__":
     main()
